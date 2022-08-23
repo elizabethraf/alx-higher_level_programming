645a38469af8dfd0435b26426ec7dc8270342790
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-23 12:13:53 -0700

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last_number =  number[-1:]
 if last_number < 0:
+   last_number = -last_number
+    print("last_number of {} is {} ".format(number, last_number), end+"")
+elif last_number > 5
     print(f'{last_number} and is greater than 5')
 elif last_number == 0:
     print(f'{last_number} is zero')
