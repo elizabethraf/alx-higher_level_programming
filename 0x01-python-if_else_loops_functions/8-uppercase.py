8eaed73f64e23350b3473fbcd8f18d0f68eea1ed
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-23 15:49:34 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for b in str:
+        _i = ord(b)
+        if _i >= 97 and _i <= 122:
+            char = chr (_i - 32)
+        else:
+            char = b
+        print("{:s}".format(char), end="")
+    print('')
