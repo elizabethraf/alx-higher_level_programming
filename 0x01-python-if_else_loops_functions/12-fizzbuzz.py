1c7d506ce4190f2ba461bdf8827c22d036a40536
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-23 17:11:42 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-  for number in range(1, 101):
-    if (number % 3 == 0 and number %5 != 0):
+  for number in range(1, 100):
+    if (number % 3 == 0):
         print("FizzBuzz", end=" ")
-    if (number % 5 == 0 and number % 3 != 0):
+    if (number % 15 == 0):
         print("Fizz", end=" ")
-    if( number % 5 == 0 and number % 3 == 0):
+    if( number % 5 == 0):
         print("Buzz", end=" ")
     else:
         print(number, end=" ")
