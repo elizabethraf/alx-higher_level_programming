018c88e53d395602764cb1cb229f708c4b35926c
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-25 12:25:32 -0700

    4-hidden_discovery.py codestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-import dis, types
+import dis
 import marshal as ms
 
-def stored_code(mycode, spaces=''):
+
+def stored_code(mycode):
     for const in mycode.co_consts:
-        if type(const) != types.CodeType and const is not None:
+        if not isinstance(const, CodeType) and const is not None:
             if not const.startswith("_"):
                 print(f"{const}")
+
 
 if __name__ == '__main__':
     with open('hidden_4.pyc', 'rb') as rof:
         rof.seek(16)
         store = ms.load(rof)
     stored_code(store)
-
-
