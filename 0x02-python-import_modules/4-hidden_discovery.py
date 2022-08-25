4eda26a2ddb1ed6ac90337c7938498e3fba25e32
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-25 11:58:40 -0700

    4-hidden_discovery.py indent

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ import dis
 import marshal as ms
 
 if __name__ == '__main__':
-with open('hidden_4.pyc', 'rb') as rof:
-    rof.seek(16)
-    dis.dis(ms.load(rof))
+    with open('hidden_4.pyc', 'rb') as rof:
+        rof.seek(16)
+        dis.dis(ms.load(rof))
