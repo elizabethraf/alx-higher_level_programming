7c0ebcc67432be641fc2647a3015dd240b1c462a
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-25 12:20:20 -0700

    4-hidden_discovery.py fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-import dis
+import dis, types
 import marshal as ms
+
+def stored_code(mycode, spaces=''):
+    for const in mycode.co_consts:
+        if type(const) != types.CodeType and const is not None:
+            if not const.startswith("_"):
+                print(f"{const}")
 
 if __name__ == '__main__':
     with open('hidden_4.pyc', 'rb') as rof:
         rof.seek(16)
-        dis.dis(ms.load(rof))
+        store = ms.load(rof)
+    stored_code(store)
+
+
