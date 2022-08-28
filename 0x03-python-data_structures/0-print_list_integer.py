7307db28d1bfc06d6ee81715ff93ed62c40cdccd
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-28 06:32:53 -0700

    0-print_list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    my_list = [1, 2, 3, 4, 5]
-    for a in my_list:print('{:d}'.format(a))
+    for a in my_list:
+        print('{:d}' .format(a))
