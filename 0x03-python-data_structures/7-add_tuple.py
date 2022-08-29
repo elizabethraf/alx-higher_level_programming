d93f539182f4fadae33f2268e42ab0719047b809
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-29 13:15:28 -0700

    add

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    my_tuple = ()
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    for a in tuple_a:
+        for b in tuple_b:
+            if len_a == len_b:
+                 my_tuple +=  (a + b)
+            if len_b > len_a:
+                if b.index > c.index:
+                    my_tuple += (b + 0)
+                else:
+                    my_tuple +=
+
+                my_tuple + = (0 + b)
+        print("The tuple after addition is: ")
+    print(new_tuple)
