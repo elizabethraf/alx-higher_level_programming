fb5cc10e250630ba6bb72803ddb505c29e19158d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-29 14:36:17 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == 0:
+        return None
+    large = 0
+    for idx in my_list:
+        if idx > large:
+            large = idx
+    return (large)
