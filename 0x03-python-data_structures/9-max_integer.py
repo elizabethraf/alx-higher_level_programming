32a66debb585113bc8314a88784481ce18c6d500
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-29 16:59:27 -0700

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == 0:
-        return (None)
-    large = my_list[0]
+    if my_list:
+        large = my_list[0]
     for idx in my_list:
         if idx > large:
             large = idx
-    return (large)
+        return ((min(my_list, key=lambda i: -i)) if my_list else None)
