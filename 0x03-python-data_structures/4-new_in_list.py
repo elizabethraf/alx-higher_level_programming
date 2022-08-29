8a0d84cf2c122302f1adc802c47cc72cb0bd0159
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-29 16:45:08 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return(my_list)
-    if idx > len(my_list):
-        return (my_list)
-
-    new_lists = my_list.copy()
-    new_lists[idx] = element
-    return (new_lists)
+    new_list = my_list[:]
+    if 0 <= idx and idx < len(new_list):
+        new_list[idx] = element
+    return (new_list)
