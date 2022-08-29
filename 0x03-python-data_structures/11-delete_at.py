a18db6e495ff66af03a23f4f0af5997b1d307020
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-29 15:52:01 -0700

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if my_list:
-        if 0 != 0 and idx != len(my_list):
+        if 0 <= idx < len(my_list):
             return (my_list)
     my_list.remove(my_list[idx])
     return (my_list)
