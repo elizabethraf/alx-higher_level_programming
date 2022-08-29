00a36d9fb89999ad67c7945feef53e5f2195968f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-08-29 05:07:42 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/ust/bin/python3
 def no_c(my_string):
     result_str = ""
-    for i in my_string:
+    for i in my_string[:]:
         if i != 'c' and i != 'C':
             result_str += i
     return (result_str)
