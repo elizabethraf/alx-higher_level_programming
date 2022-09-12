8db498c600344ae272ef5c1426f946f8fb9acadd
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-12 11:47:34 -0700

    4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+def list_division(my_list_1, my_list_2, list_length):
+    item = []
+    for y in range(list_length):
+        try:
+            count = my_list_1[y] / my_list_2[y]
+        except (ValueError, TypeError):
+            print("wrong type")
+            count = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            count = 0
+        except IndexError:
+            print("out of range")
+            count = 0
+
+        finally:
+            item.append(count)
+    return (item)
