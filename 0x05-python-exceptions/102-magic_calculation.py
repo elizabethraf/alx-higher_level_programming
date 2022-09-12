9a20ffe46087ee1caf7ae25581c75593a7aefbfa
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-12 14:11:37 -0700

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+    count = 0
+    for y in range(4, 6):
+        try:
+            if a < b:
+                raise Exception("Too far")
+            else:
+                count += (a ** b) / y
+         except:
+               count = b + a
+               break
+         return (count)
