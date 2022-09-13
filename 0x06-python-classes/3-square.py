1e930a8c7d1cbf3deac918e70fdafaaf220fbd7d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-13 14:21:52 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+The size of a square is crucial for a square, many things depend of it (area
+computation, etc.).
+Attributes:
+    size (int): The size of the new square.
+"""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        return: None
+        """
+        self.__size = size
+
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+
+    def area(self):
+        self.area = self.__size ** 2
+
+        return (self.area)
