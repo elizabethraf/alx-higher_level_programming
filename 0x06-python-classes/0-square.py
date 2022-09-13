187c7410bb2a941360bb1a8398dd8e2e14855f71
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-13 11:48:01 -0700

    doc sring eg

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Example Google style docstrings.
+
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+"""
+
 class Square:
     """Defines a square"""
 
     def __init__(self):
-        return None
+        """Initialising data."""
+        pass
