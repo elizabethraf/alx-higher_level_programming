cc2bd7de9acfbc55a4e8f3c8bfffbbd396ddcb65
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-19 11:23:00 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Example Google style docstrings.
+
+"""
+
+class Rectangle:
+    """Defines an empty  rectangle"""
+
+    def __init__(self):
+        """Initialising data."""
+        pass
