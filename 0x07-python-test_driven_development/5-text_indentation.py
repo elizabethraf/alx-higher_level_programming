5440863b7e34452962239dbf5ed1f2b09288ab9d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-25 13:22:57 -0700

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+prints a text with 2 new lines after each of these characters: ., ? and :
+
+"""
+
+
+def text_indentation(text):
+    """Prit a text with 2 new lines after each characters"""
+
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    flag = 0
+    for a in text:
+        if flag == 0:
+            if a == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                flag = 0
+            else:
+                print(a, end="")
