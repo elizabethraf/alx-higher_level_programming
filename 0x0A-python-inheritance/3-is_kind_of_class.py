6c0130ce7430dd2d1da933e7bd730b73106f1a21
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-26 09:48:23 -0700

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines is_kind_of_class(obj, a_class).
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """It return True if the objects is an instance, otherwise False"""
+
+    return (isinstance(obj, a_class))
