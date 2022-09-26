5f7f4bc5da0fc26dc72295583ac3a3cf586846fb
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-26 15:36:10 -0700

    9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Defines class BaseGeometry and subclass Rectangle.
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines rectangle"""
+
+    def __init__(self, width, height):
+        """initialise the rectangle"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        """Calculates area of rectangle."""
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Description of rectangle."""
+
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
