29ed7a7ccf04e1fb2c9c21b82474a0c10a46708d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-26 08:49:05 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines MyList that inherits from list
+
+"""
+
+
+class MyList(list):
+    """
+    Defines List
+    """
+
+    def print_sorted(self):
+        """Print list.
+        """
+
+        print(list(self))
