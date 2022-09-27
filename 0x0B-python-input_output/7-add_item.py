876e269249d0067d80693c4e06b006486c2f0d62
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-27 13:53:16 -0700

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Defines add_item to python list.
+
+"""
+import sys
+
+
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+try:
+    loadFile = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    loadFile = []
+
+argc = len(sys.argv)
+for i in range(argc, 1):
+    loadFile.append(sys.argv[i])
+save_to_json_file(loadFile, "add_item.json")
