8d7444b076bc7b6e1bb896a3cae5aee4b2b22190
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-10-03 21:20:06 -0700

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,3 +16,43 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Define string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save file"""
+        filename = cls.__name__ + ".json"
+        lo = []
+        if list_objs is not None:
+            for i in list_objs:
+                lo.append(cls.to_dictionary(i))
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(lo))
+
+   @staticmethod
+   def from_json_string(json_string):
+       """Json string to dictionary"""
+       if json_string is None or len(json_string) == 0:
+            return []
+       return json.loads(json_string)
+   
+   @classmethod
+   def load_from_file(cls):
+       """file to instance"""
+        filename = cls.__name__ + ".json"
+        b = []
+        try:
+            with open(filename, 'r') as f:
+                b = cls.from_json_string(f.read())
+            for i, e in enumerate(b):
+                b[i] = cls.create(**l[i])
+        except:
+            pass
+        return b
+
