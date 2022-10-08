ea8080e3d995aec68ae6947e5ec2143f55eb23db
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-10-08 06:27:35 -0700

    task is tiring

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,51 +1,62 @@
 #!/usr/bin/python3
 """
-Define Square class
+Class Rectangle inherits from Base
 """
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Define a square"""
+    """Defines an empty  square"""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """initialise the square"""
-        super().__init__(size, size, x, y, id)
+        """Initialising a sqaure"""
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         self.size = size
 
     @property
     def size(self):
-        """getter for size"""
+        """getter for the width"""
         return self.__width
+
+    @property
+    def size(self):
+        """getter for the height"""
+        return self.__height
 
     @size.setter
     def size(self, value):
-        """setter for size"""
+        """Size of the square"""
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """information of the square"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
-                                                         self.y, self.width)
-
     def update(self, *args, **kwargs):
-        """update attributes"""
-        if "id" in kwargs:
-            self.id = kwargs["id"]
-        if "size" in kwargs:
-            self.size = kwargs["size"]
-        if "x" in kwargs:
-            self.x = kwargs["x"]
-        if "y" in kwargs:
-            self.y = kwargs["y"
+        """Updating the size of the Square"""
+        if len(kwargs) is not 0:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+        elif len(args) is not 0:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                pass
         else:
-            for key, values in kwargs.items():
-            setattr(self, key, values)
+            print()
 
     def to_dictionary(self):
-        """difine dictionary"""
-        dictionary = {}
-        for index in ["id", "size", "x", "y"]:
-            dictionary[index] = getattr(self, index)
-        return dictionary
+        """Define Dictionery"""
+        return {'id': self.id, 'x': self.x, 'size': self.width, 'y': self.y}
+
+    def __str__(self):
+        '''
+        String representation
+        '''
+        return '[Square] ({}) {}/{} - {}'.format(self.id,
+                                                 self.x, self.y, self.size)
