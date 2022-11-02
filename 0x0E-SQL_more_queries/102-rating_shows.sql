730d328891688390a233f6d3c7fb061c9bdfe506
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-02 12:23:49 -0700

    102-rating_shows.sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,3 @@
+-- Import the database hbtn_0d_tvshows_rate dump to your MySQL server.
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_show_ratings JOIN tv_shows ON tv_shows.id = tv_show_ratings.show_id GROUP BY tv_shows.title ORDER BY rating DESC;
+
