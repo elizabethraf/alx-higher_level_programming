cd659a1d7e3a7c2bc0d6b5e8585c9bdede2a9fa8
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-02 04:40:01 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,6 @@
 -- creates the MySQL server user user_0d_1. 
-CREATE USER IF NOT EXISTS user_0d_1@locahost IDENTIFIED BY user_0d_1_pwd;
--- grant previlleges
-GRANTS ALL PRIVILEGES ON *.* TO user_0d_1 @localhost;
--- flush
+CREATE USER IF NOT EXISTS 'user_0d_1'@'locahost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
 FLUSH PRIVILEGES;
 
