cebf5524654ddb2caa048a5bb82d418fc55dddd4
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-02 04:20:54 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- creates the MySQL server user user_0d_1. 
-CREATE USER user_0d_1@locahost  IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@locahost  IDENTIFIED BY 'user_0d_1_pwd';
 SHOW GRANTS FOR user_0d_1 @localhost;
-
 
