af02945df0b372a497d02cc39211c5ca84319abe
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-02 06:46:30 -0700

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,6 +3,6 @@
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.genre_id IS NULL
+WHERE tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
 
