82a559f843478cf7414e5aab7de0f0e5124ff5a4
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-02 12:21:07 -0700

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,3 @@
  -- Import the database dump from hbtn_0d_tvshows to your MySQL server.
- SELECT title FROM tv_shows WHERE title NOT IN (
-SELECT s.title FROM tv_genres g, tv_show_genres t, tv_shows s
-WHERE g.id = t.genre_id
-    AND t.show_id = s.id
-    AND g.name = "Comedy"
-) ORDER BY title ASC;
-;
+ SELECT tv_shows.title FROM tv_shows WHERE tv_shows.title NOT IN (SELECT tv_shows.title FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = "Comedy") ORDER BY tv_shows.title;
 
