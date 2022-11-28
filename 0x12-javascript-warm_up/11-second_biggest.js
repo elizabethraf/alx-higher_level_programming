4c3ddce744980b3e627ad69700a4c10e0b03afa8
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-28 12:35:05 -0800

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv;
-  console.log(list.sort((a, b) => a - b).slice(5)[1]);
+  const list = process.argv.sort();
+  console.log(list.reverse()[1]);
 }
