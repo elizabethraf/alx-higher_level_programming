0bc02005cc56dc47297bde0c64acb0d8d3492ced
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-28 09:25:13 -0800

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+if (process.argv[2] === undefined) {
+  console.log('Not a number');
+} else if (process.argv.length === 3) {
+  console.log(process.argv[2] + 'My number: 89');
+} else if (process.argv.length === 4) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+}
