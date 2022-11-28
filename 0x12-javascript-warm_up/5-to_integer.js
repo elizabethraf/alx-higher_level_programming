4e84453876eef032de6795f456f0d5649f271b89
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-28 12:08:44 -0800

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
-if (process.argv[2] === NaN) {
+if (isNaN(parseInt(process.argv[2])) === false) {
   console.log('Not a number');
 } else if (process.argv.length === 3) {
   console.log(process.argv[2] + 'My number: 89');
