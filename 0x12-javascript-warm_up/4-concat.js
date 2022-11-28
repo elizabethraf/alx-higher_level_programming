9edc04c48c016de1a6df23bd30a8a4b18a78266c
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-28 08:44:07 -0800

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,6 +2,8 @@
 const process = require('process');
 if (process.argv[2] === undefined) {
   console.log('undefined is undefined');
-} else {
-  console.log(process.argv[2]);
+} else if (process.argv.length === 3) {
+  console.log(process.argv[2] + ' is undefined');
+} else if (process.argv.length === 4) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
