4a6015da86322e2ffa8cad662411a0a5cd3327f8
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-28 11:52:30 -0800

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
 let count;
+let wordc;
+let myword = '';
 if (process.argv.length > 2) {
   for (count = 0; count < parseInt(process.argv[2]); count++) {
-    console.log('X'.repeat(parseInt(process.argv[2])));
+    for (wordc = 0; wordc < parseInt(process.argv[2]); wordc++) {
+      myword += 'X';
+    }
+    console.log(myword);
+    myword = '';
   }
 } else {
-  console.log('Missing size');
+  console.log('Missing number of occurrences');
 }
