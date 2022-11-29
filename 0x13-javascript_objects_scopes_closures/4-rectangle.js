ac90a8a9509573f797579e8282dc7d437493b76f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-29 06:57:18 -0800

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let a = 0; a < this.height; a++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+rotate();
+this.width = this.width;
+this.height += this.height;
+this.width -= this.height;
+
+double();
+this.width = this.width * 2;
+this.height = this.height * 2;
+
+module.exports = Rectangle;
