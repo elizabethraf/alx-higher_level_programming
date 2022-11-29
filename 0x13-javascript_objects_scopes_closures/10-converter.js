ed528efdc4fbf4552ab28a389f1d0e152f9b2067
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-29 12:49:26 -0800

    10-converter.js

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  if (base === 10) {
+    return function (convNumber) {
+      return (convNumber).toString(base);
+    }
+  } else {
+    return function (convNumber) {
+      return parseInt(convNumber, base);
+    }
+};
