db5e175b45c863ace45a568ec590e475a74fa53f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-29 05:30:04 -0800

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log('X');
+  }
+}
+
+module.exports = Rectangle;
