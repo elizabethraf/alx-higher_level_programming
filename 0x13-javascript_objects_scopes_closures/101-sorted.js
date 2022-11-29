0b13370e6c8ac1ff3a1f449e17d80040c96abeff
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-29 13:36:50 -0800

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,6 +3,9 @@ const dict = require('./101-data.js').dict;
 console.log(dict);
 const newdict = {};
 Object.keys(dict).map(function (num, index) {
-  return num * index;
+  if (newdict[dict[key]] === undefined) {
+    newdict[dict[key]] = [];
+  }
+  newdict[dict[key]].push(key);
 });
 console.log(newdict);
