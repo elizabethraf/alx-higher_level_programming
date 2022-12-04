2d0b0bc18195d074854bd71c27b5c4dbc38f7b3c
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-12-04 09:11:02 -0800

    read amd0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Lists of all states from the database.
+
+"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    database = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2],
+                                sys.argv[3])
+
+    selector = database.cursor()
+    selector.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+    database.close()
