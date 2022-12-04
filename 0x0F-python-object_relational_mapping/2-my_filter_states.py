bda567719f884590d7bbea9df0b72e04e7412a76
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-12-04 11:37:36 -0800

    fixede-2_my

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     database = MySQLdb.connect("localhost",
                                sys.argv[1], sys.argv[2], sys.argv[3])
     query = "SELECT id, name FROM states\
-            WHERE name = '{}' ORDER BY states.id ASC".format(sys.argv[4])
+            WHERE name = '{}'".format(sys.argv[4])
 
     selector = database.cursor()
     selector.execute(query)
-    for state in selector.fetchone():
+    for state in selector.fetchall():
         print(state)
     selector.close()
     database.close()
