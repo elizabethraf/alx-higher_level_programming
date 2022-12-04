2fac0ab4def64c2014bc2da362a70c37b94d886e
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-12-04 10:01:47 -0800

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Lists of all states from the database.
+
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect("localhost",
+                               sys.argv[1], sys.argv[2], sys.argv[3])
+    query = "SELECT * FROM states\
+             WHERE states.name LIKE BINARY 'N%'\
+             ORDER BY states.id ASC"
+    selector = database.cursor()
+    selector.execute(query)
+    for state in selector.fetchall():
+        print(state)
+    selector.close()
+    database.close()
