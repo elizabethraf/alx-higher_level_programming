7587cc5590b0676e2987f3746e30a7b09d384629
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-12-15 10:13:34 -0800

    1-body.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL.
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
+curl -sI -XGET "$1" | grep "Content-Length" | cut -d " " -f2
 
