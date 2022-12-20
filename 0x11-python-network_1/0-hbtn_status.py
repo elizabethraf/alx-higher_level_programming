042e0d006861faeb382bfc8ca22e5af79ac77d6c
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-12-20 05:32:17 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
 Define script that fetches https://alx-intranet.hbtn.io/status.
-
 """
 import urllib.request
+
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
