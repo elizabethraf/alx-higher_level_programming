c03fc1352a95dbce7fbf7a6381e512cbdf6b1766
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-17 11:10:36 -0800

    3-starwarsfix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const episodeNumber = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${episodeNumber}`;
 
 request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
+  if (!error && response.statusCode === 200) {
     const data = JSON.parse(body);
     console.log(data.title);
   }
