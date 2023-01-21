132754dc2dd42070571ad7e81b28cdfb774a98c0
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-21 09:35:31 -0800

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const args = process.argv;
 
 fs.writeFile(args[2], args[3], 'utf-8', (err, data) => {
-    if (err) { console.log(err); }
+  if (err) { console.log(err); }
 });
