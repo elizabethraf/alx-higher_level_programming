bcf9a3f6eb36eba557085bf27796cb79a80615b0
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-22 07:04:56 -0800

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
-const async = require('async');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const movieId = 3; // Example: 3 = "Return of the Jedi"
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
 request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const film = JSON.parse(body);
-    console.log(`Characters in ${film.title} :`);
-    async.mapSeries(film.characters, (characterUrl, callback) => {
-      request(characterUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const character = JSON.parse(body);
-          callback(null, character.name);
-        } else {
-          callback(`Error requesting character: ${error}`);
-        }
-      });
-    }, (err, results) => {
-      if (err) {
-        console.log(err);
-      } else {
-        results.forEach(name => console.log(name));
-      }
-    });
-  } else {
-    console.log(`Error requesting film: ${error}`);
+  if (error) {
+    console.error('Error:', error);
+    return;
   }
+  if (response.statusCode !== 200) {
+    console.error('Invalid status code:', response.statusCode);
+    return;
+  }
+  const json = JSON.parse(body);
+  const characters = json.characters;
+  characters.forEach(character => {
+    console.log(character);
+  });
 });
