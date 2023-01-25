09840f301fcad48fa8cd41820067e75ecbe00336
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-25 14:54:48 -0800

    fixed

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,11 +1,11 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
 <script>
-$(document).ready(function() {
-  $('#btn_translate'.click(function() {
-    var language_code = $('#language_code').val();
-    var api_url = 'https://www.fourtonfish.com/hellosalut/hello/';
-    $.get(api_url + language_code, function(data) {
-      $("#hello").html(data.hello);
+$(document).ready(() => {
+  $('#btn_translate').click(() => {
+    let language_code = $('#language_code').val();
+    const api_url = 'https://www.fourtonfish.com/hellosalut/hello/';
+    $.get(`${api_url}${language_code}`, (data) => {
+      $('#hello').html(data.hello);
       });
     });
 });
--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,19 +1,19 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
 <script>
 $(document).ready(() => {
-  $("#btn_translate").click(() => {
+  $('#btn_translate').click(() => {
     getTranslation();
     });
-  $("#language_code").keypress((e) => {
+  $('#language_code').keypress((e) => {
     if(e.which === 13) {
       getTranslation();
     }
     });
   function getTranslation() {
-    let language_code = $("#language_code").val();
-    const api_url = "https://www.fourtonfish.com/hellosalut/hello/";
+    let language_code = $('#language_code').val();
+    const api_url = 'https://www.fourtonfish.com/hellosalut/hello/';
     $.get(`${api_url}${language_code}`, (data) => {
-      $("#hello").html(data.hello);
+      $('#hello').html(data.hello);
       });
   }
 });
